18349775c0f1d0b4e0741a94c9702abde2e1df4c
Forrest Smith <fsmith2017@gmail.com>
2017-10-17 00:01:51 -0400

    pa2 start
    Created outline for all functions and arr struct

--- pa2/learn/learn.c
+++ pa2/learn/learn.c
@@ -10,9 +10,12 @@ int main(int argc, char** argv)
 		exit(0);
 	}
 	FillMatrix(argc, argv);
-	InvertMatrix(trainMatrix);
-	PrintMatrix();
+	//wMatrix = GetWeights();
+	//GaussEliminate(trainMatrix);
+	PrintMatrix(trainMatrix);
 	Printy();
+	Printw();
+	Printm();
 	return 0;
 }
 
@@ -21,62 +24,45 @@ void InvertMatrix(double** matrix)
 
 }
 
-void TransposeMatrix(double** matrix)
+double** TransposeMatrix(double** matrix)
 {
 
+	return NULL;
 }
 
-void FillMatrix(int argc, char** argv)
+double** GaussEliminate(double** matrix)
 {
-	int i, j, r;
-	double temp;
+	double** idMatrix;
+	idMatrix = (double**) malloc((nSize)*sizeof(double*));	// Allocate size
+	int rows, cols, i;
+	rows = sizeof(matrix)/sizeof(double**);
+	cols = sizeof(matrix[0])/sizeof(double*);
 
-	FILE* fp = NULL;
-	char* trainingName = NULL;
-	char* testingName = NULL;
-
-	trainingName = argv[1];
-	testingName = argv[2];
-
-	fp = fopen(trainingName, "r");
-	if(fp == NULL){
-		printf("error: File does not exist\n");
-		exit(0);
-	}
-
-	r = fscanf(fp, "%d", &kSize);		// Get size of values
-	printf("kSize: %d\n", kSize);
-	if(r != 1){printf("error\n"); exit(0);}		// Error throw
-
-	r = fscanf(fp, "%d", &nSize);		// Get size of values
-	printf("nSize: %d\n", nSize);
-
-	trainMatrix = (double**) malloc((nSize)*sizeof(double*));	// Allocate size
-	yMatrix = (double*) calloc((nSize),sizeof(double));
-
-	// Allocating space for the prim
-	for(i = 0; i <= nSize; i++)
+	// Allocating space for the training matrix
+	for(i = 0; i <= rows; i++)
 	{
-		trainMatrix[i] = (double*)calloc(kSize+1,sizeof(double));
-		trainMatrix[i][0] = 1;
+		idMatrix[i] = (double*) malloc((cols)*sizeof(double));
 	}
 
+	rows = rows < cols ? rows : cols;
 
-	// Entering values
-
-	for(i = 0; i < nSize; i++)
+	for(i = 0; i < rows; i++)
 	{
-		for(j = 1; j <= kSize; j++){
-			fscanf(fp, "%lf,", &temp);
-			//printf("temp: %f\n", temp);
-			trainMatrix[i][j] = temp;
-		}
-		fscanf(fp, "%lf,", &temp);
-		yMatrix[i]= temp;
+		idMatrix[i][i] = 1.0;
 	}
-	//printf("done");
 
+	PrintMatrix(idMatrix);
+
+	return NULL;
 }
+
+double* GetWeights()
+{
+	//double** invMatrix;
+	return NULL;
+}
+
+
 
 /*void FreeMemory()
 {
@@ -104,43 +90,147 @@ void AssignMatrix()
 		}
 }
  */
-void PrintMatrix()
+void PrintMatrix(double** matrix)
 {
-	int i, j;
-	//printf("%d\n", power);
-	for(i = 0; i < nSize; i++)
+	int i, j, rows, cols;
+	printf("Matrix\n");
+	rows = sizeof(matrix)/sizeof(double*);// / sizeof(matrix[0]);
+	cols = sizeof(matrix[0]);// / sizeof(matrix[0][0]);
+	printf("rows:%d, cols:%d\n", rows, cols);
+	//printf("%f\n", matrix[9][4]);
+	for(i = 0; i < 10; i++)
 	{
-		for(j = 0; j <= kSize; j++)
+		for(j = 0; j < 5; j++)
 		{
-			printf("%f", trainMatrix[i][j]);
+			printf("%f", matrix[i][j]);
 			printf("\t");
 		}
 		printf("\n");
 	}
+	printf("\n");
 }
 
 void Printy()
 {
 	int i;
-	//printf("%d\n", power);
+	printf("Y-MATRIX:\n");
 	for(i = 0; i < nSize; i++)
 	{
 		printf("%f", yMatrix[i]);
 		printf("\t");
 	}
-	printf("\n");
+	printf("\n\n");
 
+}
+
+void Printm()
+{
+	int i, j;
+	printf("M-MATRIX:\n");
+	for(i = 0; i < mSize; i++)
+	{
+		for(j = 0; j < kSize; j++)
+		{
+			printf("%f", testMatrix[i][j]);
+			printf("\t");
+		}
+		printf("\n");
+	}
+	printf("\n");
 }
 
 void Printw()
 {
 	int i;
-	//printf("%d\n", power);
-	for(i = 0; i < nSize; i++)
+	printf("W-MATRIX:\n");
+	for(i = 0; i < (kSize+1); i++)
 	{
 		printf("%f", wMatrix[i]);
 		printf("\t");
 	}
-	printf("\n");
+	printf("\n\n");
+
+}
+
+void FillMatrix(int argc, char** argv)
+{
+	int i, j, r, s;
+	double temp;
+
+	FILE* fTrain = NULL;
+	FILE* fTest = NULL;
+	char* trainingName = NULL;
+	char* testingName = NULL;
+
+	trainingName = argv[1];
+	testingName = argv[2];
+
+	fTrain = fopen(trainingName, "r");
+	if(fTrain == NULL){
+		printf("error: File does not exist\n");
+		exit(0);
+	}
+
+	fTest = fopen(testingName, "r");
+	if(fTest == NULL){
+		printf("error: File does not exist\n");
+		exit(0);
+	}
+
+	r = fscanf(fTrain, "%d\n", &kSize);		// Get k-attribute size
+	printf("kSize: %d\n", kSize);
+	if(r != 1){printf("error\n"); exit(0);}		// Error throw
+
+	r = fscanf(fTrain, "%d", &nSize);		// Get n-data size
+	printf("nSize: %d\n", nSize);
+
+	s = fscanf(fTest, "%d\n", &mSize);		// Get k-attribute size
+	printf("mSize: %d\n", mSize);
+	if(s != 1){printf("error\n"); exit(0);}		// Error throw
+
+	trainMatrix = (double**) malloc((nSize)*sizeof(double*));	// Allocate size
+	testMatrix = (double**) malloc((mSize)*sizeof(double*));	// Allocate size
+	yMatrix = (double*) calloc((nSize),sizeof(double));
+	wMatrix = (double*) calloc((kSize+1),sizeof(double));
+
+	// Allocating space for the training matrix
+	for(i = 0; i <= nSize; i++)
+	{
+		trainMatrix[i] = (double*)calloc(kSize+1,sizeof(double));
+		trainMatrix[i][0] = 1;
+	}
+
+	// Allocate space for test matrix
+	for(i = 0; i < mSize; i++)
+	{
+		testMatrix[i] = (double*)calloc(kSize,sizeof(double));
+	}
+
+
+	// Entering Train values
+
+	for(i = 0; i < nSize; i++)
+	{
+		for(j = 1; j <= kSize; j++){
+			fscanf(fTrain, "%lf,", &temp);
+			//printf("temp: %f\n", temp);
+			trainMatrix[i][j] = temp;
+		}
+		fscanf(fTrain, "%lf,", &temp);
+		yMatrix[i]= temp;
+	}
+
+	// Entering Test values
+
+	for(i = 0; i < mSize; i++)
+	{
+		for(j = 0; j < kSize; j++){
+			fscanf(fTest, "%lf,", &temp);
+			//printf("temp: %f\n", temp);
+			testMatrix[i][j] = temp;
+		}
+	}
+	fclose(fTrain);
+	fclose(fTest);
 
 }
--- pa2/learn/learn.h
+++ pa2/learn/learn.h
@@ -1,10 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double *wmatrix = NULL;
+double *wMatrix = NULL;
 double *yMatrix = NULL;
 double **trainMatrix = NULL;
+double **testMatrix = NULL;
 int kSize, nSize, mSize;
+
+struct arr{
+    int rows;
+    int cols;
+    double **matrix ; // change the member!
+};
+
+typedef struct arr arr;
+
+arr* create_arr(int rows, int cols, int train)
+{
+	arr *rMatrix = malloc(sizeof *rMatrix);
+	rMatrix->matrix = (double**) malloc((rows)*sizeof(double*));
+	int i;
+	switch(train)
+	{
+	case 1:
+		for(i = 0; i <= rows; i++)
+		{
+			trainMatrix[i] = (double*)calloc(cols,sizeof(double));
+			trainMatrix[i][0] = 1;
+		}
+		break;
+	case 0:
+		for(i = 0; i < rows; i++)
+		{
+			trainMatrix[i] = (double*)calloc(cols,sizeof(double));
+		}
+		break;
+	}
+	return rMatrix;
+}
 
 #ifndef LEARN_H_
 #define LEARN_H_
@@ -13,15 +46,21 @@ void FillMatrix(int argc, char** argv);
 
 void InvertMatrix(double** matrix);
 
-void TransposeMatrix(double** matrix);
+double** TransposeMatrix(double** matrix);
+
+double** GaussEliminate(double** matrix);
+
+double* GetWeights();
 
 void FreeMemory();
 
 void AssignMatrix();
 
-void PrintMatrix();
+void PrintMatrix(double** matrix);
+
 
 void Printy();
 void Printw();
+void Printm();
 
 #endif
